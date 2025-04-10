97fc02962efbf1d93cd7b3ce35ddece6c6418014
Ibrahim <elkadiri.ibrahim@hotmail.com>
2025-04-10 04:05:04 +0200

    rework navbar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -10,12 +10,64 @@ import { ScrollToDirective } from '../shared/scroll-to.directive';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  mobileMenuOpen = false;
+  isLangMenuOpen = false;
+  ismobileMenuOpen = false;
 
-  darkMode = false;
+  menuItems = [
+    {
+      label: 'Acceuil', id: 'home'
+    },
+    {
+      label: 'Présentation', id: 'about'
+    },
+    {
+      label: 'Services', id: 'services'
+    },
+    {
+      label: 'Expertises', id: 'expertises'
+    },
+    {
+      label: 'Contact', id: 'contact'
+    },
+  ]
+  languages = [
+    {
+      label: 'English (US)',
+      code: 'en',
+      icon: `<circle fill="#b22234" cx="8" cy="8" r="8"></circle>`,
+    },
+    {
+      label: 'Deutsch',
+      code: 'de',
+      icon: `<rect fill="#000" width="16" height="5"></rect>
+             <rect fill="#d00" y="5" width="16" height="5"></rect>
+             <rect fill="#ffce00" y="10" width="16" height="5"></rect>`,
+    },
+    {
+      label: 'Italiano',
+      code: 'it',
+      icon: `<rect fill="#009246" width="5" height="16"></rect>
+             <rect fill="#fff" x="5" width="5" height="16"></rect>
+             <rect fill="#ce2b37" x="10" width="5" height="16"></rect>`,
+    },
+  ];
 
-  toggleDarkMode() {
-    this.darkMode = !this.darkMode;
-    document.documentElement.classList.toggle('dark', this.darkMode);
+  selectedLanguage = this.languages[0];
+
+
+  toggleMobileMenu() {
+    this.isLangMenuOpen = false;
+    this.ismobileMenuOpen = !this.ismobileMenuOpen;
+  }
+  toggleLangMenu() {
+    this.ismobileMenuOpen = false;
+    this.isLangMenuOpen = !this.isLangMenuOpen;
+  }
+
+  selectLanguage(lang: any) {
+    this.selectedLanguage = lang;
+    this.isLangMenuOpen = false;
+    console.log('Langue sélectionnée :', lang.code);
+    // Ici, tu pourrais appeler un service de traduction (i18n)
   }
 }
